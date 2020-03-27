1b442d6829b06f53f77adec5abab4f7a4799f7b3
Pierre Julien <pierrejulienpro@outlook.fr>
2020-03-27 13:48:50 +0100

    Add project files.

---
+++ Hardware/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Hardware.Models;
+
+namespace Hardware.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
