57074b959c46b78f429bdc2d497adcccc4f3dc00
Pierre Julien <pierrejulienpro@gmail.com>
2022-03-02 21:21:41 +0100

    Suppression des permissions Android dans le projet Forms

--- Hardware/AssemblyInfo.cs
+++ Hardware/AssemblyInfo.cs
@@ -1,13 +1,3 @@
-using Android.App;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-[assembly: UsesPermission(Android.Manifest.Permission.BatteryStats)]
-[assembly: UsesPermission(Android.Manifest.Permission.Vibrate)]
-[assembly: UsesPermission(Android.Manifest.Permission.AccessCoarseLocation)]
-[assembly: UsesPermission(Android.Manifest.Permission.AccessFineLocation)]
-[assembly: UsesFeature("android.hardware.location", Required = false)]
-[assembly: UsesFeature("android.hardware.location.gps", Required = false)]
-[assembly: UsesFeature("android.hardware.location.network", Required = false)]
-[assembly: UsesFeature("android.hardware.camera", Required = true)]
-[assembly: UsesFeature("android.hardware.camera.autofocus", Required = true)]
